6e7d0c6329efc7d88b29467f7a42e1fd4d3483eb
techLinky <gcebile.sne@gmail.com>
2023-09-20 12:42:12 +0200

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        txtID = New TextBox()
+        txtVerify = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(92, 64)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(75, 25)
+        Label1.TabIndex = 0
+        Label1.Text = "Enter ID"
+        ' 
+        ' txtID
+        ' 
+        txtID.Location = New Point(400, 64)
+        txtID.Name = "txtID"
+        txtID.Size = New Size(150, 31)
+        txtID.TabIndex = 1
+        ' 
+        ' txtVerify
+        ' 
+        txtVerify.Location = New Point(156, 189)
+        txtVerify.Name = "txtVerify"
+        txtVerify.Size = New Size(112, 34)
+        txtVerify.TabIndex = 2
+        txtVerify.Text = "Verify"
+        txtVerify.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtVerify)
+        Controls.Add(txtID)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents txtVerify As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Text.RegularExpressions
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class Form1
+    Private Sub txtVerify_Click(sender As Object, e As EventArgs) Handles txtVerify.Click
+        Dim idNumber As String = txtID.Text.Trim()
+
+        If IsSouthAfricanID(idNumber) Then
+            MessageBox.Show("The ID number is correct.")
+        Else
+            MessageBox.Show("The ID number is correct.")
+        End If
+    End Sub
+
+    Private Function IsSouthAfricanID(idNumber As String) As Boolean
+        ' Check if the ID number is exactly 13 digits long and contains only numeric characters.
+        If idNumber.Length = 13 AndAlso Integer.TryParse(idNumber, 0) Then
+            Return True
+        Else
+            Return False
+        End If
+    End Function
+
+End Class
